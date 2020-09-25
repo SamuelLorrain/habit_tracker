106122fcc3a518254a15f500b3bfcbb5e79ef6c4
SamuelLorrain <samuel.lorrain@protonmail.com>
2020-09-25 21:35:54 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use chrono::{DateTime, FixedOffset, Duration, Utc, Local};
+
+struct Habit {
+    name: String,
+    date_begin: Option<DateTime<Local>>,
+    date_end: Option<DateTime<Local>>,
+}
+
+impl Habit {
+}
+
+fn main() {
+}
