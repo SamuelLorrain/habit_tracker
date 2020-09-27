e49a69798433cf686929acd745af0cd4488b2f17
SamuelLorrain <samuel.lorrain@protonmail.com>
2020-09-27 20:49:10 +0200

    Improve overall habit implementation (not perfect)

--- src/habit.rs
+++ src/habit.rs
@@ -1,91 +1,161 @@
-use chrono::{DateTime, FixedOffset, Duration};
-use std::fmt;
+use chrono::{NaiveDateTime, Duration, Utc};
 
 #[derive(Debug)]
 pub struct Habit {
     name: String,
-    date_begin: Option<DateTime<FixedOffset>>,
-    date_end: Option<DateTime<FixedOffset>>,
-    interval: Option<Duration>,
-    done: bool,
-    metadata: Option<String>
+    metadata: Option<String>,
+
+    date_begin: NaiveDateTime,
+    interval: Duration,
+
+    history: Vec<(NaiveDateTime,    // first: last_date_done
+                  NaiveDateTime,    // second: real_last_date_done
+                  Option<String>)>, // third: metadata
+
+    //times_missed: i32,
+    active: bool
 }
 
+//getters
 impl Habit {
-    pub fn new(name: String,
-               date_begin: Option<DateTime<FixedOffset>>,
-               date_end: Option<DateTime<FixedOffset>>,
-               interval: Option<Duration>,
-               metadata: Option<String>) -> Habit{
-        Habit{
-            name,
-            date_begin,
-            date_end,
-            interval,
-            done: false,
-            metadata
+    pub fn name         (&self) -> &String           { &self.name }
+    pub fn metadata     (&self) -> &Option<String>   { &self.metadata }
+    pub fn date_begin   (&self) -> &NaiveDateTime    { &self.date_begin }
+    pub fn interval     (&self) -> &Duration         { &self.interval }
+    pub fn times_done   (&self) -> usize             { self.history.len() }
+    //pub fn times_missed (&self) -> i32 {
+    //    self.update_times_missed();
+    //    self.times_missed
+    //}
+    pub fn active       (&self) -> bool              { self.active }
+    pub fn last_done    (&self) -> Option<&(NaiveDateTime,
+                                            NaiveDateTime,
+                                            Option<String>)>
+                                                     { self.history.last() }
+
+}
+
+//setters
+impl Habit {
+    pub fn set_name(&mut self, name : &str) {
+        self.name.clear();
+        self.name = String::from(name);
+    }
+
+    pub fn set_metadata(&mut self, data : &str) {
+        if data == "" {
+            self.metadata = None;
+        } else {
+            self.metadata = Some(String::from(data));
         }
     }
 
-    pub fn check_end_date(&mut self, &date_end: &DateTime<FixedOffset>) -> Result<&mut Habit, &str> {
-        match self.date_begin {
-            None => Ok(self),
-            Some(beg) => {
-                if(beg.timestamp() - date_end.timestamp()) < 0 { //diff
-                    self.date_end = Some(date_end.clone());
-                    return Ok(self);
-                }
-                Err("The date is bad")
-            }
-        }
+    pub fn set_date_begin(&mut self, new_date: &NaiveDateTime) -> &NaiveDateTime {
+        self.date_begin = new_date.clone();
+        &self.date_begin
     }
 
-    pub fn check_begin_date(&mut self, &date_begin: &DateTime<FixedOffset>) -> Result<&mut Habit, &str> {
-        match self.date_end {
-            None => Ok(self),
-            Some(end) => {
-                if(end.timestamp() - date_begin.timestamp()) < 0 { //diff
-                    self.date_begin = Some(date_begin.clone());
-                    return Ok(self);
-                }
-                Err("The date is bad")
-            }
+    pub fn set_interval(&mut self, new_interval: Duration) -> Result<&Duration, &str> {
+        if new_interval < Duration::zero() {
+            return Err("Unable to have a negative interval");
+        }
+        self.interval = new_interval;
+        Ok(&self.interval)
+    }
+
+    pub fn set_active(&mut self, is_active: bool) -> Result<bool, &str> {
+        if self.date_begin < Utc::now().naive_utc() {
+            self.active = is_active;
+            Ok(self.active)
+        } else {
+            Err("Unable to change active")
         }
     }
 }
 
-impl fmt::Display for Habit {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let date_begin = match self.date_begin {
-            Some(x) => x.to_string(),
-            None => String::from("")
-        };
+// métier
+impl Habit {
 
-        let date_end = match self.date_end {
-            Some(x) => x.to_string(),
-            None => String::from("")
-        };
+    pub fn next_date_done(&self) -> Result<NaiveDateTime, &'static str> {
+        match self.active {
+            false => Err("The habit is not active"),
+            true => {
+                let mut date_done = self.date_begin.clone();
+                let now = Utc::now().naive_utc();
+                while date_done < now {
+                    date_done = date_done + self.interval
+                }
+                Ok(date_done)
+            }
+        }
+    }
 
-        let interval = ""; //No proper implementation yet
+    pub fn has_to_be_done(&self) -> bool {
+        match self.active {
+            false => false,
+            true => {
+                let now = Utc::now().naive_utc();
+                match self.history.last() {
+                    None => {
+                        if now > self.date_begin {
+                            true
+                        } else {
+                            false
+                        }
+                    },
+                    Some(last_done) => {
+                        if now > last_done.1 {
+                            true
+                        } else {
+                            false
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-        let done = match self.done {
-            false => "It has not been done.",
-            true  => "It has been done.",
-        };
+    pub fn done(&mut self, metadata: Option<String>) -> Result<&(NaiveDateTime, NaiveDateTime, Option<String>), &'static str>{
+        if self.active == false {
+            return Err("Unable to done an unactive habit");
+        }
 
-        let metadata = match &self.metadata {
-            Some(x) => x.to_string(),
-            None => String::from("")
-        };
+        if ! self.has_to_be_done() {
+            return Err("Unable to done a habit that has already been done");
+        }
+        let next_date_done = self.next_date_done().unwrap();
+        let real_next_date_done = Utc::now().naive_utc();
+        self.history.push((next_date_done, real_next_date_done , metadata));
+        Ok(&self.history.last().unwrap())
+    }
 
-        write!(f, "===========\n\
-                  name: {}\n\
-                  begin: {}\n\
-                  end: {}\n\
-                  interval: {}\n\
-                  {}\n\
-                  metadata: {}\n\
-                  ===========",
-                  self.name, date_begin, date_end, interval, done, metadata)
+    //pub fn update_times_missed() -> i32 {
+    //    let now = Utc::now().naive_utc();
+    //    let date = self.date_begin;
+    //    while date < self.now {
+    //        date += self.interval
+    //    }
+    //}
+}
+
+impl Default for Habit {
+    fn default() -> Self {
+        let date_begin = Utc::now().naive_utc();
+        let interval = Duration::zero();
+        let history: Vec<(NaiveDateTime, NaiveDateTime, Option<String>)> = Vec::new();
+        let name = String::new();
+
+        Habit {
+            name: name,
+            metadata: None,
+
+            date_begin: date_begin,
+            interval: interval,
+
+            history: history,
+
+            times_missed: 0,
+            active: false
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 
-mod habit;
+pub mod habit;
 
 use chrono::{DateTime, FixedOffset};
 
 use habit::{Habit};
-use std::ptr;
 
 #[derive(Debug)]
 struct Sprint {
@@ -15,75 +14,21 @@ struct Sprint {
     metadata: Option<String>
 }
 
-impl Sprint {
-    fn new(name: Option<String>,
-           date_begin: DateTime<FixedOffset>,
-           date_end: DateTime<FixedOffset>,
-           metadata: Option<String>) -> Sprint {
-        Sprint {
-            name,
-            habits: Vec::new(),
-            date_begin,
-            date_end,
-            metadata
-        }
-    }
-
-    fn add_habit(&mut self, h: Habit){
-        self.habits.push(h);
-    }
-
-    fn delete_habit(&mut self, h: &Habit){
-        let index = self.habits.iter().position(|x| ptr::eq(x, h))
-            .expect("Element invalid");
-        self.habits.remove(index);
-    }
-}
-
-//impl Display for Sprint {
-//    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//    }
-//}
-
-
 fn main() {
     let date_begin = DateTime::parse_from_rfc3339("2020-09-19T16:39:57-08:00")
         .expect("Test");
     let date_end = DateTime::parse_from_rfc3339("2020-10-19T16:39:57-08:00")
         .expect("Test");
 
-    let new_date_end = DateTime::parse_from_rfc3339("2020-11-19T16:39:57-08:00")
-        .expect("Test");
+    let mut h = Habit::default();
 
-    let new_date_begin = DateTime::parse_from_rfc3339("2020-08-19T16:39:57-08:00")
-        .expect("Test");
+    //let mut s = Sprint::new(
+    //    None,
+    //    date_begin,
+    //    date_end,
+    //    Some(String::from("Test"))
+    //);
+    //s.add_habit(h);
 
-    let mut h = Habit::new(
-        String::from("Test"),
-        Some(date_begin),
-        Some(date_end),
-        None,
-        None,
-    );
-
-    match h.check_end_date(&new_date_end) {
-        Ok(_) => println!("Date successfully changed"),
-        Err(x) => eprintln!("{}", x)
-    }
-
-    match h.check_begin_date(&new_date_begin) {
-        Ok(_) => println!("Date successfully changed"),
-        Err(x) => eprintln!("{}", x)
-    }
-
-    let mut s = Sprint::new(
-        None,
-        date_begin,
-        date_end,
-        Some(String::from("Test"))
-    );
-
-    s.add_habit(h);
-
-    println!("{:?}", s);
+    println!("{:?}", h);
 }
